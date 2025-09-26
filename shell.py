34ce0b39188bff81fa3807157368864b2f43a55a
lachameleon <hogridersupercell123@gmail.com>
2025-09-26 16:18:13 +1000

    Implemented cd, fixed multithreading issues, and added a handler for when KeyboardInterrupt exception is detected.

--- shell.py
+++ shell.py
@@ -6,6 +6,10 @@ import os
 import time
 import config
 import platform
+import threading
+import platform
+import time
+import os
 
 command = 'Woo, lachadev dc!'
 noescape = False
@@ -20,7 +24,7 @@ def play_sound(path):
         subprocess.run(["aplay", path])
     else:
         raise OSError("Unsupported OS")
-    
+
 def clear_screen():
     subprocess.run(["clear"])
 
@@ -68,58 +72,39 @@ def run_music():
     subprocess.run('python3 music.py', shell=True, text=True)
 
 def handle_command(cmd):
+    global cwd
+    parts = cmd.strip().split()
+    if not parts:
+        return
+
+    if parts[0] == "cd":
+        # Handle 'cd' commands
+        if len(parts) == 1:
+            # Just 'cd' with no argument → go home
+            home_dir = os.path.expanduser("~")
+            os.chdir(home_dir)
+        else:
+            try:
+                os.chdir(os.path.expanduser(parts[1]))
+            except FileNotFoundError:
+                print(f"No such directory: {parts[1]}")
+            except Exception as e:
+                print(f"Error changing directory: {e}")
+        cwd = os.getcwd()
+        return
+
+    # Existing commands
     if cmd == 'setuser':
         set_user()
     elif cmd in ['quit', 'q']:
         clear_screen()
         time.sleep(0.3)
         run_animation()
+        global running
+        running = False
         exit()
     elif cmd == 'hi':
-        print("""
-⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀
-⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀
-⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀
-⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷
-⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀⠤⠀⢲⡖⣂⣀⠀⠀⢙⣶⣄⠈⠉⣸⡄⠠⣠⡿
-⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁
-⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀
-⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀
-⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁              
-              
-             """)
-        print('Hear a knock at your door?')
-        play_sound('uhoh.wav')
-        subprocess.run('clear', shell=True, text=True)
         print('hi :)')
-        cwd = os.getcwd()
-        cwd = input(f"\033[1;32m{user}\033[0m:\033[1;34m{cwd}\033[0m > ")
-        print('Just kidding...')
-        print("""
-⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀
-⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀
-⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀
-⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷
-⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀⠤⠀⢲⡖⣂⣀⠀⠀⢙⣶⣄⠈⠉⣸⡄⠠⣠⡿
-⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁
-⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀
-⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀
-⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁              
-              
-             """)
-        print("You can't escape me...")
-        play_sound('uhoh.wav')
-        subprocess.run('clear', shell=True, text=True)
-        print('hi :)')
-        noescape = True
     elif cmd == 'playsound':
         toggle_playsound()
     elif cmd == 'quickanimation':
@@ -128,7 +113,6 @@ def handle_command(cmd):
         subprocess.run('python3 help.py', shell=True, text=True)
     elif cmd == 'music':
         print('Music is disabled right now due to difficulty implementing.')
-        # run_music()
     elif cmd == 'who ate the barber':
         subprocess.run('clear', shell=True, text=True)
         print('It was your father...')
@@ -147,7 +131,6 @@ def handle_command(cmd):
 state = config.state
 user = state["user"]
 cwd = os.getcwd()
-running = True
 hasansweredplaysound = False
 hasanswredquickanimation = False
 
@@ -156,10 +139,15 @@ run_animation()
 print('Welcome back', user + '!')
 print('Easy to use daily driver shell made for non-devs.')
 
-command = input('What would you like to run first? > ')
-handle_command(command)
-
-while running:
+# Main shell loop
+while True:
     cwd = os.getcwd()
-    command = input(f"\033[1;32m{user}\033[0m:\033[1;34m{cwd}\033[0m > ")
-    handle_command(command)
+    try:
+        command = input(f"\033[1;32m{user}\033[0m:\033[1;34m{cwd}\033[0m > ")
+        handle_command(command)
+    except KeyboardInterrupt:
+        subprocess.run('python3 animation.py', shell=True, text=True)
+        subprocess.run('clear', shell=True, text=True)
+        print('Seeya!')
+        running = False
+        break
